c9f20868b5d662b9302e2ac9b920e7ac33e49b07
teiatan <92535482+teiatan@users.noreply.github.com>
2022-09-01 22:15:11 +0300

    Changing js-file mobile menu button began chacging icon correctly

--- js/mobile-header.js
+++ js/mobile-header.js
@@ -4,7 +4,7 @@
   
     menuBtnRef.addEventListener("click", () => {
       const expanded =
-        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+        menuBtnRef.getAttribute("aria-expanded") === "two" || one;
   
       menuBtnRef.classList.toggle("is-open");
       menuBtnRef.setAttribute("aria-expanded", !expanded);
